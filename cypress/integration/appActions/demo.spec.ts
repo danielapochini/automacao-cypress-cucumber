47032ad903e040a2ec7214a29159e1fc16a03a05
danielapochini <daniela.pochini@fatec.sp.gov.br>
2022-09-01 19:44:06 -0300

    removed unused spec files

--- cypress/integration/appActions/demo.spec.ts
+++
@@ -1,9 +0,0 @@
-describe('Demo Test', () => {
-	it('Should visit the login page', () => {
-		cy.visitLoginPage()
-		cy.waitForSeconds(5)
-		cy.fixture('loginData').then(({ username, password }) => {
-			cy.login(username, password)
-		})
-	})
-})
--- cypress/integration/appActions/feedback.spec.ts
+++
@@ -1,8 +0,0 @@
-describe('Feedback Test', () => {
-	it('Should submit feedback form', () => {
-		cy.visitFeedbackPage()
-		cy.fixture('feedbackData').then(({ name, email, subject, message }) => {
-			cy.submitFeedback(name, email, subject, message)
-		})
-	})
-})
